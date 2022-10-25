9dbb04d244c9c29412196be568aa444bab7c2418
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-10-25 19:48:01 -0400

    added Employee class and tests for the methods and functions

--- index.js
+++ index.js
@@ -3,39 +3,8 @@ const fs = require('fs');
 const Employee = require("./lib/employee");
 const generateHTML = require("./dist/generateHTML");
 
-// data - questions
 
-const questions = [
-    {
-        type: 'input',
-        message: 'What is the name of the employee?',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'What is their employee ID?',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'What is their email address?',
-        name: 'email',
-    },
-    {
-        type: 'list',
-        message: 'What is their role?',
-        name: 'role',
-        choices: ['Manager', 'Engineer', 'Intern']
-    },
-]
 
-// functions - create HTML file
-// generateHTML functio needs to be created somewhere
-inquirer.prompt(questions)
-.then((response) => {
-    // fs.writeFile('index.html', generateHTML(response), (err) =>
-    // err ? console.error(err) : console.log('Success!'))
-})
 
 // initiliza new employee object
 // const employee = new Employee();
---
+++ tests/employee.test.js
@@ -0,0 +1,50 @@
+
+const Employee = require('../lib/employee');
+
+describe("Employee Class", () => {
+    describe("Initialization", () => {
+        it("should make an object with name, id and email properties", () => {
+            // ARRANGE
+            const employee = new Employee("Laura", "11", "lcfhines@gmail.com")
+            // ACT
+            // ASSERT
+            expect(employee).toEqual({
+                name: "Laura",
+                id: "11",
+                email: "lcfhines@gmail.com"
+            })
+        })
+    })
+    describe("getName", () => {
+        it("should return the name of an employee", () => {
+            const name = "Laura"
+            const employee = new Employee(name);
+            expect(employee.getName()).toBe(name);
+            
+        })
+    })
+    describe("getID", () => {
+        it("should return the ID of an employee", () => {
+            const id = "123"
+            const employee = new Employee("Laura", id);
+            expect(employee.getId()).toBe(id)
+            
+        })
+    })
+    describe("getEmail", () => {
+        it("should return the email of an employee", () => {
+            const email = "test@email.com"
+            const employee = new Employee("Laura", "123", email);
+            expect(employee.getEmail()).toBe(email)
+            
+        })
+    })
+    describe("getRole", () => {
+        it("should return the role of an employee", () => {
+            const role = "Employee"
+            const employee = new Employee("Laura", "123", "test@email.com");
+            expect(employee.getRole()).toBe(role)
+            
+        })
+    })
+})
